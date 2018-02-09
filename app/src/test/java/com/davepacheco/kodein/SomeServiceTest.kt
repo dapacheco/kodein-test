ccf05b9dffde62da666298c4b1415101e7571616
David Pacheco <david.pacheco@sky.uk>
2018-02-09 09:17:34 +0000

    Update test to verify override of dependency in a test.

--- app/src/test/java/com/davepacheco/kodein/SomeServiceTest.kt
+++ app/src/test/java/com/davepacheco/kodein/SomeServiceTest.kt
@@ -1,15 +1,24 @@
 package com.davepacheco.kodein
 
+import com.davepacheco.kodein.services.MessageService
 import com.github.salomonbrys.kodein.Kodein.Module
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.singleton
+import com.github.salomonbrys.kodein.with
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 
 val testModule = Module {
-    bind<ServiceDependency>(overrides = true) with singleton { ServiceDependency("TestValue") }
+    bind<MessageService>(overrides = true) with singleton { DefaultMessageService(instance(tag = "defaultGreeting")) }
+    constant("defaultGreeting", overrides = true) with "TestValue"
+}
+
+class DefaultMessageService(private val defaultMessage: String): MessageService {
+    override fun getMessage(): String {
+        return defaultMessage
+    }
 }
 
 class SomeServiceTest {
@@ -18,10 +27,10 @@ class SomeServiceTest {
     @JvmField
     val injectorRule = KodeinRule(::MainApplication, listOf(testModule))
 
-    val serviceTest: SomeService by injectorRule.injector.instance()
+    val serviceTest: MessageService by injectorRule.injector.instance()
 
     @Test
     fun `ensure Kodein dependency can be overriden`() {
-        Assert.assertEquals(serviceTest.dependencyValue(), "TestValue")
+        Assert.assertEquals(serviceTest.getMessage(), "TestValue")
     }
 }
